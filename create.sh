32807eae4fa062c1517f018c13cb23b8599c4e35
neo <neocarton@gmail.com>
2021-11-07 20:11:45 +0700

    Fix code

--- create.sh
+++ create.sh
@@ -1,6 +1,6 @@
 docker create --name nanominer \
     -p 9090:9090 \
-    -v "/home/neo/Programs/nanominer/config:/nanominer-linux-3.4.2-cuda11/config" \
+    -v "/home/neo/Programs/docker-nanominer-nvidia/config:/nanominer-linux-3.4.2-cuda11/config" \
     --restart=always \
     --memory=4g \
     --memory-swap=8g \
